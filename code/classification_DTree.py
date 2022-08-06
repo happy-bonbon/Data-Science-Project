a1f09267c6e1bfef97f550085cb7a6f6dc2fb44e
happy-bonbon <happy.yux@gmail.com>
2022-08-06 17:19:55 +1000

    notebook files and plot added

--- code/classification_DTree.py
+++ code/classification_DTree.py
@@ -17,42 +17,38 @@ def main():
     
     classlabel = dataset['max_price_category']
 
-    # parameter tuning with diferent folds
-    for k in range(10,51,10):
-        # K-fold Method (cross validation) with Classification Accuracy Formula
-        kf = KFold(n_splits = k, shuffle = True, random_state = 88)
+    # K-fold Method (cross validation) with Classification Accuracy Formula
+    k, x_times = 20, 12
+    kf = KFold(n_splits = k, shuffle = True, random_state = 88)
+    classification_accuracy = []
+    
+    # Implementation of K-fold Method
+    for train_index, test_index in kf.split(features):
         
-        # x_times is the max depth of the Decision Tree Classifier (parameter tuning)
-        for x_times in range(10, 13):
-            classification_accuracy = []
-            
-            # Implementation of K-fold Method
-            for train_index, test_index in kf.split(features):
-                
-                # Split Training and Test sets
-                features_train, feature_test = features.iloc[train_index, :], features.iloc[test_index, :]
-                class_train, class_test = classlabel[train_index], classlabel[test_index]
-                
-                # Scaling the features
-                scalar = preprocessing.StandardScaler().fit(features_train)
-                features_train = scalar.transform(features_train)
-                feature_test = scalar.transform(feature_test)
-            
-                # Step 1: Instantiate 
-                dt = DecisionTreeClassifier(
-                    criterion='entropy', random_state=88, max_depth = x_times)
-                
-                # Step 2: Fit
-                dt.fit(features_train, class_train)
-                
-                # Step 3: Predict
-                predictions = dt.predict(feature_test)
-                
-                # Step 4: Evaluate
-                my_score=accuracy_score(class_test, predictions)
-                classification_accuracy.append(my_score)
-            print(f'The AVG {k}-Fold Accuracy Score with Max depth of {x_times} is {sum(classification_accuracy)/k}')
+        # Split Training and Test sets
+        features_train, feature_test = features.iloc[train_index, :], features.iloc[test_index, :]
+        class_train, class_test = classlabel[train_index], classlabel[test_index]
         
+        # Scaling the features
+        scalar = preprocessing.StandardScaler().fit(features_train)
+        features_train = scalar.transform(features_train)
+        feature_test = scalar.transform(feature_test)
+    
+        # Step 1: Instantiate 
+        dt = DecisionTreeClassifier(
+            criterion='entropy', random_state=88, max_depth = x_times)
+        
+        # Step 2: Fit
+        dt.fit(features_train, class_train)
+        
+        # Step 3: Predict
+        predictions = dt.predict(feature_test)
+        
+        # Step 4: Evaluate
+        my_score=accuracy_score(class_test, predictions)
+        classification_accuracy.append(my_score)
+    print(f'The AVG {k}-Fold Accuracy Score with Max depth of {x_times} is {sum(classification_accuracy)/k}')
+    
 
 if __name__ == "__main__":
     main()
--- code/classification_Knn.py
+++ code/classification_Knn.py
@@ -3,12 +3,10 @@ from sklearn import neighbors
 from sklearn import preprocessing
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
 
 def main():
-    '''classification for Max Price Category VS Weather O(n**3) Best: 10-fold 8-NN'''
+    '''classification for Max Price Category VS Weather Best: 10-fold 8-NN'''
     
     # read csv file
     dataset = pd.read_csv('Data/combined_detail_cleaned.csv')
@@ -19,40 +17,37 @@ def main():
     
     classlabel = dataset['max_price_category']
 
-    # parameter tuning with diferent folds
-    for k in range(10,51,10):
     # K-fold Method (cross validation) with Classification Accuracy Formula
-        kf = KFold(n_splits = k, shuffle = True, random_state = 88)
+    k, my_neighbors = 10, 8
+    kf = KFold(n_splits = k, shuffle = True, random_state = 88)
+
+    classification_accuracy = []
     
-        # my_neighbors is the value of K Neighbors Classifier (parameter tuning)
-        for my_neighbors in range(5,11):
-            classification_accuracy = []
-            
-            # Implementation of K-fold Method
-            for train_index, test_index in kf.split(features):
-                
-                # Split Training and Test sets
-                features_train, feature_test = features.iloc[train_index, :], features.iloc[test_index, :]
-                class_train, class_test = classlabel[train_index], classlabel[test_index]
-                
-                # Scaling the features
-                scalar = preprocessing.StandardScaler().fit(features_train)
-                features_train = scalar.transform(features_train)
-                feature_test = scalar.transform(feature_test)
-            
-                # Step 1: Instantiate 
-                knn = neighbors.KNeighborsClassifier(n_neighbors = my_neighbors)
-                
-                # Step 2: Fit
-                knn.fit(features_train, class_train)
-                
-                # Step 3: Predict
-                predictions = knn.predict(feature_test)
-                
-                # Step 4: Evaluate
-                my_score=accuracy_score(class_test, predictions)
-                classification_accuracy.append(my_score)
-            print(f'The AVG {k}-fold Accuracy Score with K-neighbors = {my_neighbors} is {sum(classification_accuracy)/k}')
+    # Implementation of K-fold Method
+    for train_index, test_index in kf.split(features):
+        
+        # Split Training and Test sets
+        features_train, feature_test = features.iloc[train_index, :], features.iloc[test_index, :]
+        class_train, class_test = classlabel[train_index], classlabel[test_index]
+        
+        # Scaling the features
+        scalar = preprocessing.StandardScaler().fit(features_train)
+        features_train = scalar.transform(features_train)
+        feature_test = scalar.transform(feature_test)
+    
+        # Step 1: Instantiate 
+        knn = neighbors.KNeighborsClassifier(n_neighbors = my_neighbors)
+        
+        # Step 2: Fit
+        knn.fit(features_train, class_train)
+        
+        # Step 3: Predict
+        predictions = knn.predict(feature_test)
+        
+        # Step 4: Evaluate
+        my_score=accuracy_score(class_test, predictions)
+        classification_accuracy.append(my_score)
+    print(f'The AVG {k}-Fold Accuracy Score with {my_neighbors}-neighbors is {sum(classification_accuracy)/k}')
 
 
 if __name__ == "__main__":
